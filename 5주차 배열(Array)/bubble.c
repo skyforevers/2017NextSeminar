a2de15de57e7f511ed8da1497af70e96c6461c4f
HYJin <sky64100@naver.com>
2017-05-24 21:31:57 +0900

    5주차 배열 과제

---
+++ 5주차 배열(Array)/bubble.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define MAX 10
+
+void Bubble(int a[]){
+  // bubble sort 코드 구현
+
+}
+
+void print(int a[]){
+  // 배열을 출력하는 함수
+}
+
+int main(){
+  int array[MAX] = {2,7,8,6,5,4,3,1,10,9};
+  // 정렬전에 출력
+  // 정렬
+  // 정렬후에 출력
+  return 0;
+}
