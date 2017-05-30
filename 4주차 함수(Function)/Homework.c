fa9abf0432b2302add3b99a174d72ea47c40e9ae
HYJin <sky64100@naver.com>
2017-05-30 23:47:34 +0900

    4th week : Function

---
+++ 4주차 함수(Function)/Homework.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int factorial(int n) {
+    // 해당되는 팩토리얼 파트를 구성하시오
+}
+
+int main() {
+    
+    printf("Factorial Result (5!) = %d\n",factorial(5));
+
+    return 0;    
+}
+
+// Main 함수에는 손을 대지 마시오
+// Factorial 함수를 완성하여 5! = 120 이라는 결과값이 나오게 출력하시오
+// 메인 함수에서 입출력을 해서 변형을 해도 좋음
