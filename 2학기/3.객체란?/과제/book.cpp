54c9f55416627101153f76cff817ff93b3eeefe0
ChanggunLee <cd8114@gmail.com>
2017-08-07 18:00:08 +0900

    add new files 3weeks

---
+++ 2학기/3.객체란?/과제/book.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+class book{
+private:
+  string name; // 책 이름
+  int page; // 페이지수
+  int price; // 가격
+  int buy; // 구매수
+
+public:
+  //class안에 메소드 정의만 실행문은 class 밖에 작성할것
+
+  //생성자 이름과 페이지수 가격 초기화
+  // 구매수는 0으로 초기화
+  book();
+
+  // 소멸자 소멸시 "소멸"출력
+  ~book();
+
+  // 책을 한권을 구매하는 메소드
+  // buy가 1 증가한다
+  // 안내문도 출력할것
+  int buy_book();
+
+  // 정보를 출력하는 메소드
+  void print();
+
+}
+
+// 메소드 작성 필요
+
+int main(){
+
+  // 하나의 인스턴스를 생성
+  book  *b; // (수정 필요)
+
+  // b의 정보를 출력
+  b->print();
+
+  // b를 5번 구매 하기(수정 필요)
+  b->buy_book();
+
+  // b의 정보를 출력
+  b->print();
+
+  return 0;
+}
---
+++ 2학기/3.객체란?/실습/class.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+
+class student{
+private:
+  string name; // 이름
+  int age; // 나이
+  int *score; // 3개의 과목의 성적
+  double avg; // 평균
+
+public:
+  //class안에 메소드 정의만 실행문은 class 밖에 작성할것
+
+  //생성자 이름과 나이 초기화
+  student();
+
+  // 소멸자 소멸시 "소멸"출력
+  ~student();
+
+  // 정보를 입력하는 메소드
+  int input();
+
+  // 정보를 출력하는 메소드
+  void printf();
+
+  // 평균을 구하는 메소드
+  void getavg();
+
+}
+
+int main(){
+
+  // 하나의 인스턴스를 생성
+  studnet stu;
+
+  // 입력
+  stu.input();
+
+  // 평균 구하기
+  stu.getavg();
+
+  // 출력
+  stu.print();
+
+  return 0;
+}
