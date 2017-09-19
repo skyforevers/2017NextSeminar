ccd40c5d706130769b5be22dc5b52472cce2b6a9
ChanggunLEE <cd8114@gmail.com>
2017-09-20 00:32:05 +0900

    add code 2.ex

--- 2학기/2.c++문법/실습/inout.cpp
+++ 2학기/2.c++문법/실습/inout.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
@@ -6,8 +7,11 @@ int main(){
   int age; // 나이
 
   // 이름 나이 입력
-
+    cin >> name;
+    cin >> age;
   // 이름 나이 출력
+    cout << name << endl;
+    cout << age << endl;
 
   return 0;
 }
--- 2학기/2.c++문법/실습/newdelete.cpp
+++ 2학기/2.c++문법/실습/newdelete.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main(){
@@ -7,12 +8,26 @@ int main(){
   int *score; // 국어 , 영어 점수 입력
 
   // new로 할당 받기
+    name = new string;
+    age = new int;
+    score = new int[2];
 
   // 이름 나이 점수 입력
+    cin >> *name;
+    cin >> *age;
+    cin >> score[0];
+    cin >> score[1];
 
   // 이름 나이 점수 출력
+    cout << *name << endl;
+    cout << *age << endl;
+    cout << score[0] << endl;
+    cout << score[1] << endl;
 
   // delete로 반환
+    delete name;
+    delete age;
+    delete[] score;
 
   return 0;
 }
