98b58211d1531dc7ffebbde05c24941e90bd824f
LEEChangGun <cd8114@gmail.com>
2017-11-08 11:25:48 +0900

    add exmaple

---
+++ 2학기/5.다형성/animal.cpp
@@ -0,0 +1,26 @@
+#include "iostream"
+#include "stdlib"
+#include "string"
+
+class animal{
+    protected:
+        int leg;
+        string name;
+        int age;
+    public:
+        animal();
+        animal(string s);
+        makenosie();
+    
+};
+
+
+int main(){
+
+
+}
+
+
+
+
+
